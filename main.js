917cef263040b10eb1e7ff3d70ef4420e57834d2
gukanozadze <gukanozadze@gmail.com>
2019-12-09 17:25:54 +0400

    final commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// // font is Relative to container's width
+// // defined font-size will not affect to element after uncommented
+// jQuery(".card__title").fitText(1);
+// jQuery(".card__subtitle").fitText(1.2);
+
