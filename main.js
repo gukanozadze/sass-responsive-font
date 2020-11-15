070138d6f6f1dba5d3cae6d1334ed37880af9123
guka <gukanozadze@gmail.com>
2020-11-15 19:25:45 +0400

    feat: removed node modules

--- main.js
+++
@@ -1,6 +0,0 @@
-
-// // font is Relative to container's width
-// // defined font-size will not affect to element after uncommented
-// jQuery(".card__title").fitText(1);
-// jQuery(".card__subtitle").fitText(1.2);
-
